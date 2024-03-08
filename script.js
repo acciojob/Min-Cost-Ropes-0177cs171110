75cd161bd352ce036ec2af9a418d597abafedfe1
Rahul Kumar <90969936+0177cs171110@users.noreply.github.com>
2024-03-08 10:24:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 function mincost(arr) {
-    const Heap = require('collections/heap'); // Importing min-heap from collections library
-
-    // Initialize a min heap to store the lengths of the ropes
-    const minHeap = new Heap(arr);
-
-    let cost = 0;
-
-    // Connect the ropes until there is only one rope left
-    while (minHeap.length > 1) {
-        // Extract the two shortest ropes from the min heap
-        const shortest1 = minHeap.pop();
-        const shortest2 = minHeap.pop();
-
-        // Calculate the cost of connecting these ropes
-        const currentCost = shortest1 + shortest2;
-
-        // Add the current cost to the total cost
-        cost += currentCost;
-
-        // Add the connected rope back to the min heap
-        minHeap.push(currentCost);
-    }
-
-    return cost;
+     let pq = [];
+   
+        // Adding items to the pQueue
+        for (let i = 0; i < arr.length; i++) {
+            pq.push(arr[i]);
+        }    
+           
+        pq.sort(function(a,b){return a-b;});
+         
+        // Initialize result
+        let res = 0;
+   
+        // While size of priority queue
+        // is more than 1
+        while (pq.length > 1) {
+            // Extract shortest two ropes from pq
+            let first = pq.shift();
+            let second = pq.shift();
+   
+            // Connect the ropes: update result
+            // and insert the new rope to pq
+            res += first + second;
+            pq.push(first + second);
+            pq.sort(function(a,b){return a-b;});
+        }
+   
+        return res;
 }
 
 module.exports = mincost;
